bd12ce696cd28f4fb124225d1493fe3778ad6dbc
borislav nikolov <jack@sofialondonmoskva.com>
2020-05-15 22:23:41 +0200

    better example

--- cmd/main.go
+++ cmd/main.go
@@ -15,8 +15,13 @@ func main() {
 	flag.Parse()
 
 	gemini.HandleFunc("/example", func(w *gemini.Response, r *gemini.Request) {
-		w.SetStatus(gemini.StatusSuccess, "text/gemini")
-		w.Write([]byte("HELLO: " + r.URL.Path + "\n"))
+		if len(r.URL.RawQuery) == 0 {
+			w.SetStatus(gemini.StatusInput, "what is the answer to the ultimate question")
+		} else {
+			w.SetStatus(gemini.StatusSuccess, "text/gemini")
+			answer := r.URL.RawQuery
+			w.Write([]byte("HELLO: " + r.URL.Path + ", yes the answer is: " + answer))
+		}
 	})
 
 	gemini.Handle("/", gemini.FileServer(*root))
